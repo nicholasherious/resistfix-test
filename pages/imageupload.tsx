539da42e9bbeef66f6f7a343b034350d0d403fa5
nicholasherious <nherious@gmail.com>
2022-07-14 00:10:39 -0700

    o

---
+++ pages/imageupload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageUpload from '../components/ImageUpload';
+
+function imageupload() {
+  return (
+    <div>
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default imageupload;
