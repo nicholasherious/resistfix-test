3da54c27e5ae90530f1a4ab5e3c6d8dd62ac3196
nherious <nherious@gmail.com>
2022-09-02 03:44:48 -0700

    type error fixed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 
-import { GET_ALL_POSTS } from '../graphql/queries';
-import Post from './Post';
-import Loading from './Loading/Loading';
+import { GET_ALL_POSTS } from "../graphql/queries";
+import Post from "./Post";
+import Loading from "./Loading/Loading";
 
 type Props = {
   post: Post[];
@@ -12,15 +12,25 @@ type Props = {
 function Feed() {
   const { data, loading, error } = useQuery(GET_ALL_POSTS);
 
-  if (loading)
-    return (
-      <Loading />
-    );
+  if (loading) return <Loading />;
   if (error) return <p>Oh no... {error.message}</p>;
   return (
     <div className="">
-      {data?.posts.map(post => (
-        <Post key={post.id} post={post} />
+      {data?.posts.map((post: any) => (
+        <Post
+          key={post.id}
+          post={post}
+          isSinglePage={{
+            id: "",
+            title: "",
+            url: "",
+            description: "",
+            imageUrl: "",
+            category: "",
+            comments: [],
+            isSinglePage: "",
+          }}
+        />
       ))}
     </div>
   );
