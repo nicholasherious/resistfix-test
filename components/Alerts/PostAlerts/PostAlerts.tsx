ff60c171477e7099531e79a0752629407a09193e
nherious <nherious@gmail.com>
2022-09-02 03:36:23 -0700

    fix biding type

--- components/Alerts/PostAlerts/PostAlerts.tsx
+++ components/Alerts/PostAlerts/PostAlerts.tsx
@@ -1,18 +1,17 @@
-import { Alert } from '@mantine/core';
-import { AlertCircle } from 'tabler-icons-react';
+import { Alert } from "@mantine/core";
+import { AlertCircle } from "tabler-icons-react";
 
-export function PostWarningAlert({ alertMessage}) {
+export function PostWarningAlert({ alertMessage }: any) {
   return (
     <div className="p-2">
-    <Alert
-      icon={<AlertCircle size={16} />}
-      title='Bummer!'
-      color='red'
-      radius='md'
-      
-    >
-      {alertMessage}
-    </Alert>
+      <Alert
+        icon={<AlertCircle size={16} />}
+        title="Bummer!"
+        color="red"
+        radius="md"
+      >
+        {alertMessage}
+      </Alert>
     </div>
   );
 }
